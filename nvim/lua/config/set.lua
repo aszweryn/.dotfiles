0bc35203333a46e7163325a189990ba3859c01d2
aszweryn <101809061+aszweryn@users.noreply.github.com>
2022-09-11 19:26:52 +0200

    theme, remap and tweaks for nvim

---
+++ nvim/lua/config/set.lua
@@ -0,0 +1,24 @@
+local set = vim.opt
+
+vim.opt.guicursor = ""
+
+set.nu = true
+set.relativenumber = true
+
+set.expandtab = true
+set.smarttab = true
+set.shiftwidth = 4
+set.tabstop = 4
+set.softtabstop = 4
+
+set.hlsearch = false
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+set.smartindent = true
+
+set.wrap = false
+
+vim.g.mapleader = " "
+
