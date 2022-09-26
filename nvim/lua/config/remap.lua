20f70874a740e75e9250a87972a7ccf919a63501
aszweryn <101809061+aszweryn@users.noreply.github.com>
2022-09-26 08:15:13 +0200

    telescope tweak

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -8,8 +8,8 @@ nnoremap("<leader>nt", "<cmd>NvimTreeToggle<CR>")
 nnoremap("<leader>nf", "<cmd>NvimTreeFocus<CR>")
 
 -- Telescope
-nnoremap ("<leader>ff", "<cmd>Telescope find_files<cr>")
-nnoremap ("<leader>fg", "<cmd>Telescope live_grep<cr>")
-nnoremap ("<leader>fb", "<cmd>Telescope buffers<cr>")
-nnoremap ("<leader>fh", " <cmd>Telescope help_tags<cr>")
+nnoremap ("<leader>ff", "<cmd>Telescope find_files hidden=true<cr>")
+nnoremap ("<leader>fg", "<cmd>Telescope live_grep hidden=true<cr>")
+nnoremap ("<leader>fb", "<cmd>Telescope buffers hidden=true<cr>")
+nnoremap ("<leader>fh", " <cmd>Telescope help_tags hidden=true<cr>")
 
