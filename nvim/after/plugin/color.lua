a7f56c77b0318ad417c3bb1391631f40f33308c2
aszweryn <{ID}+{username}@users.noreply.github.com>
2022-10-03 22:36:13 +0200

    various cosmetic edits

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -3,9 +3,9 @@
 require("tokyonight").setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+  style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
   transparent = true, -- Enable this to disable setting the background color
-  transparent_statusbar = true, -- Enable this to disable setting the statusbar color
+  transparent_statusbar = false, -- Enable this to disable setting the statusbar color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
     -- Style to be applied to different syntax groups
@@ -18,7 +18,7 @@ require("tokyonight").setup({
     sidebars = "transparent", -- style for sidebars, see below
     floats = "transparent", -- style for floating windows
   },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  sidebars = { "qf", "help" }, -- Set a darker transparentbackground on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
   hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
   dim_inactive = false, -- dims inactive windows
