a0901761d7ac7649ec5f17e54604ddb5069a6895
aszweryn <101809061+aszweryn@users.noreply.github.com>
2022-12-12 22:45:12 +0100

    feat: mason, lsp overhaul

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -3,7 +3,6 @@ vim.cmd("packadd packer.nvim")
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'folke/tokyonight.nvim'
-	use 'navarasu/onedark.nvim'
 	use 'EdenEast/nightfox.nvim'
 	use {
 		'nvim-treesitter/nvim-treesitter',
@@ -12,9 +11,9 @@ return require('packer').startup(function(use)
 	use {
 		'kyazdani42/nvim-tree.lua',
 		requires = {
-			'kyazdani42/nvim-web-devicons', -- optional, for file icons
+			'kyazdani42/nvim-web-devicons',
 		},
-		tag = 'nightly' -- optional, updated every week. (see issue #1193)
+		tag = 'nightly'
 	}
 	use {
 		'nvim-lualine/lualine.nvim',
@@ -24,17 +23,7 @@ return require('packer').startup(function(use)
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-	use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-	use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
 	use 'lukas-reineke/indent-blankline.nvim'
-	use {
-		"cuducos/yaml.nvim",
-		ft = {"yaml"}, -- optional
-		requires = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-telescope/telescope.nvim" -- optional
-		},
-	}
 	use {
 	"windwp/nvim-autopairs",
 	config = function() require("nvim-autopairs").setup {} end
