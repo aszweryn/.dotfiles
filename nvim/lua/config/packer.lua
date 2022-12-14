fa7587f984ecbacd7588fa87074b5c1ca320f778
aszweryn <101809061+aszweryn@users.noreply.github.com>
2022-12-14 12:48:57 +0100

    fix: better config descriptions, small tweaks

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -2,12 +2,12 @@ vim.cmd("packadd packer.nvim")
 
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
-	use 'folke/tokyonight.nvim'
-	use 'EdenEast/nightfox.nvim'
+	-- Syntax highlighting
 	use {
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate'
 	}
+	-- File explorer
 	use {
 		'kyazdani42/nvim-tree.lua',
 		requires = {
@@ -15,22 +15,30 @@ return require('packer').startup(function(use)
 		},
 		tag = 'nightly'
 	}
+	-- Pretty status bar
 	use {
 		'nvim-lualine/lualine.nvim',
 		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 	}
+	-- Ripgrep-based filtering tool
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-	use 'lukas-reineke/indent-blankline.nvim'
-	use {
-	"windwp/nvim-autopairs",
-	config = function() require("nvim-autopairs").setup {} end
-	}
+	-- All LSP-related pluggins
 	use {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"neovim/nvim-lspconfig",
 	}
+	-- Listchars, indentation helper
+	use 'lukas-reineke/indent-blankline.nvim'
+	-- Autopairing brackets
+	use {
+	"windwp/nvim-autopairs",
+	config = function() require("nvim-autopairs").setup {} end
+	}
+	-- Themes
+	use 'folke/tokyonight.nvim'
+	use 'EdenEast/nightfox.nvim'
 end)
